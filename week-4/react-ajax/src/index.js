dd99d173f30af28fd6665c3e41dc5ca4408c247a
Eric Tsai <eric492718@gmail.com>
2023-04-24 17:22:07 +0800

    Modify homework

--- week-4/react-ajax/src/index.js
+++ week-4/react-ajax/src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import {createRoot} from "react-dom/client";
-import "./index.css";
-import Home from "./pages/Home";
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import Home from './pages/Home';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Home />
